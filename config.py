f970ef5a3b6bda3b25094eefec6b65c5b6343464
wdblackjesus <35409628+wdblackjesus@users.noreply.github.com>
2018-01-14 04:00:16 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 #config file bzd_channel_bot
 #ver_0.1
 
-_TOKEN = "514947066:AAHjBq9IIENMyG0hAWXX2pvDJ7mBN7JdHq8";
+_TOKEN = "514947066:AAHjBq9IIENMyG0hAWXX2pvDJ7mBN7JdHq8"
 
-_FATHERid = 416097504;
+_FATHERid = 416097504
 
-_databaseName = "ololo";
+_databaseName = "ololo"
